6ad49a1a4db7c6a011621c7c6044f4554bae415c
22030683 <22030683@hakim>
2023-05-26 11:52:16 +0800

    first push to P000 repository in GitHub

---
+++ P000/src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b; //hakim1
+	}
+	public int substract(int a, int b) {
+		return a - b;
+	}
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
